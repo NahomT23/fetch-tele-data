15948705d0486e7607841cf2f25f25867a63e6ac
Nahom Tsegaye <tnahom214@gmail.com>
2025-01-22 18:04:15 +0300

    proper types added

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,11 +1,12 @@
 import cloudinary from "cloudinary";
 
 
+
 // Configure Cloudinary
 cloudinary.v2.config({
-  cloud_name: dxhm4bjs2,
-  api_key: 972412855347386,
-  api_secret: R19mEf5jIVqPCMM-GjV1kbgmqJg,
+  cloud_name: 'dxhm4bjs2',
+  api_key: '972412855347386',
+  api_secret: 'R19mEf5jIVqPCMM-GjV1kbgmqJg',
 });
 
 
