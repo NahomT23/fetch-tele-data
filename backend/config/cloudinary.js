ec2c38283164e7cf8207ba2dd6ecf620eeca2d8b
Nahom Tsegaye <tnahom214@gmail.com>
2025-01-22 17:55:41 +0300

    api_secret added

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -2,9 +2,11 @@ import cloudinary from "cloudinary";
 
 
 // Configure Cloudinary
-cloudinary.config({
+cloudinary.v2.config({
   cloud_name: dxhm4bjs2,
   api_key: 972412855347386,
   api_secret: R19mEf5jIVqPCMM-GjV1kbgmqJg,
 });
+
+
 
