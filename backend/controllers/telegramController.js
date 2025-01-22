2b6c89badc260189919e7485c2ea6025d09eb7a6
Nahom Tsegaye <tnahom214@gmail.com>
2025-01-22 17:29:38 +0300

    cloudinary image url test

--- backend/controllers/telegramController.js
+++ backend/controllers/telegramController.js
@@ -1,18 +1,5 @@
-// export const getFileUrl = async (fileId, BOT_TOKEN) => {
-//     try {
-//       const filePathResponse = await axios.get(
-//         `https://api.telegram.org/bot${BOT_TOKEN}/getFile?file_id=${fileId}`
-//       );
-//       const filePath = filePathResponse.data.result.file_path;
-//       return `https://api.telegram.org/file/bot${BOT_TOKEN}/${filePath}`;
-//     } catch (error) {
-//       console.error("Error fetching image:", error);
-//       return null;
-//     }
-//   };
-  
+import axios from "axios";
 
-import axios from 'axios'
 
 export const getFileUrl = async (fileId, BOT_TOKEN) => {
   try {
