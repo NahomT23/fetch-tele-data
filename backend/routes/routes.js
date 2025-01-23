60a24217841c007657331c580733f4f21773e197
Nahom Tsegaye <tnahom214@gmail.com>
2025-01-23 13:44:15 +0300

    check out session test

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.post("/getMessages", createItem);
 router.get("/items", getItems);
 router.get("/items/:id", getItemById);
-router.post("/create-checkout-session", createCheckoutSession);
+// router.post("/create-checkout-session", createCheckoutSession);
 
 
 export default router;
